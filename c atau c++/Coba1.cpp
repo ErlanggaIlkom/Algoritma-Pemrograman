b1ad61dbf1b9583a8eb697ef2e1302f0060c15ff
ErlanggaIlkom <gunawanerlanggamuhammad@gmail.com>
2022-10-13 11:26:50 +0700

    Menambahkan File Test-Git

---
+++ c atau c++/Coba1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    int a = 8;
+    int b = 1;
+    char c[9]= "Erlangga";
+    
+    puts("Nama");
+    puts(c);
+    printf("Terdiri dari %d Huruf dan %d kata ",a,b);
+}
---
+++ c atau c++/coba2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+char nm1[50];
+char nm2[50];
+
+puts("Masukan nama pertama =");puts(nm1);
+printf("masukan nama kedua =");scanf("%",&nm2);
+
+printf("\n\n");
+}
